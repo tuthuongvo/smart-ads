4e1842c8685928a09d0654d1575c529a00ee609c
Thuong Vo <thuongvo@coffeemugvn.com>
2024-09-28 16:54:54 +0700

    updated style authorize page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
       white: "#ffffff",
     },
     extend: {
+      borderColor: {
+        input: "#E5E7EB",
+      },
       colors: {
         primary: {
           50: "#eff6ff",
@@ -22,7 +25,7 @@ module.exports = {
           400: "#60a5fa",
           500: "#3b82f6",
           600: "#2563eb",
-          700: "#1d4ed8",
+          700: "#1A56DB",
           800: "#1e40af",
           900: "#1e3a8a",
           950: "#172554",
@@ -62,42 +65,8 @@ module.exports = {
       },
     },
     fontFamily: {
-      body: [
-        "Inter",
-        "ui-sans-serif",
-        "system-ui",
-        "-apple-system",
-        "system-ui",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "Noto Sans",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-        "Noto Color Emoji",
-      ],
-      sans: [
-        "Inter",
-        "ui-sans-serif",
-        "system-ui",
-        "-apple-system",
-        "system-ui",
-        "Segoe UI",
-        "Roboto",
-        "Helvetica Neue",
-        "Arial",
-        "Noto Sans",
-        "sans-serif",
-        "Apple Color Emoji",
-        "Segoe UI Emoji",
-        "Segoe UI Symbol",
-        "Noto Color Emoji",
-      ],
+      inter: ["Inter", "sans-serif"],
     },
   },
   plugins: [],
 };
-
